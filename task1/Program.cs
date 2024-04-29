be2b3d9eb5521e183617f6efc695b6ca45deb772
Vladimir <vlnshapat@gmail.com>
2024-04-29 13:11:30 +0300

    add task1 format

--- task1/Program.cs
+++ task1/Program.cs
@@ -5,8 +5,8 @@
 Console.WriteLine("ввод числа");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int number1 = number%7;
-int number2 = number%23;
+int number1 = number % 7;
+int number2 = number % 23;
 int result = number1 + number2;
 
 if (result == 0)
